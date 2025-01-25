e446000418d7d6c9077ace2de373996008a117f7
Sadab Uz Zaman <57608969+Sadab-7878@users.noreply.github.com>
2025-01-25 13:56:27 +0600

    nav done 100%

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const hamburger = document.querySelector('#hamburger');
-const dropdown = document.querySelector('.dropdown-menu');
+const hamburger = document.querySelector("#hamburger");
+const dropdown = document.querySelector(".dropdown-menu");
 
-hamburger.addEventListener('click', ()=>{
-    dropdown.classList.toggle('show')
-    console.log(dropdown)
+hamburger.addEventListener("click", () => {
+  dropdown.classList.toggle("show");
+  console.log(dropdown);
 
-   
-})
+  if (dropdown.classList.contains("show")) {
+    hamburger.src = "assets/wrong.png";
+  } else {
+    hamburger.src = "assets/hamburger.png";
+  }
+});
